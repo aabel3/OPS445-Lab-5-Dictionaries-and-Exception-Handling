47c0734ebf6a16dd5421357e7a10206c9d28ad7e
Avraham Abel <avraham.lewis.abel@gmail.com>
2025-12-04 10:35:07 -0500

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+# OPS445 - Lab 5
+# temp2.py
+# Author: Avraham Abel
 
 file = open("file1.txt", "w")
-
 file.write('Line1\nLine2 is a little longer \nLine3 is too\n')
-
 file.close()
 
 
